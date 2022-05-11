dcee2675f0b14c73bcd99aaff0fc27905fe38d39
Rahayus12 <102126439+Rahayus12@users.noreply.github.com>
2022-05-11 14:36:19 +0700

    new items

---
+++ 14_MathSqrt.java
@@ -0,0 +1,6 @@
+// metode mengembalikan akar kuadrat dari x
+public class 14_MathSqrt {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(64));
+    }
+}
---
+++ 16_IfStatement.java
@@ -0,0 +1,7 @@
+public class 16_IfStatement {
+    public static void main(String[] args) {
+        if (20 > 18) {
+            System.out.println("20 is better than 18");
+        }
+    }
+}
