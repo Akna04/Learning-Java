adbff17d309c6cae342b32c22f8424dd4cc44c62
Rahayus12 <102126439+Rahayus12@users.noreply.github.com>
2022-05-11 11:59:02 +0700

    java operator

---
+++ 8_WideCasting.java
@@ -0,0 +1,9 @@
+public class 8_WideCasting {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+  }
+}
