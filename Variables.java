2afdedfd56e3462f928f87e657b089f965fa6b27
Rahayus12 <102126439+Rahayus12@users.noreply.github.com>
2022-05-11 17:11:31 +0700

    all items already changed

---
+++ Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+  public static void main(String[] args) {
+    String firstName = "Rahayu";
+    String lastName = "Safitri";
+    System.out.println("Selamat datang " + firstName + " " + lastName);
+  }
+}
+
---
+++ Variables2.java
@@ -0,0 +1,9 @@
+public class Variables2 {
+  public static void main(String[] args) {
+    String firstName = "Ji ";
+    String middleName = "Chang ";
+    String lastName = "Wook";
+    String fullName = firstName + middleName + lastName;
+    System.out.println("Selamat datang "+ fullName);  
+  }
+}
