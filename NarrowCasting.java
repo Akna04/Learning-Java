54ae667d645b1051f336e65b67ae1d5a4b1ac17d
Rahayu Safitri <102126439+Rahayus12@users.noreply.github.com>
2022-05-17 14:23:32 +0700

    Revert "Revert "all items already changed""

---
+++ NarrowCasting.java
@@ -0,0 +1,9 @@
+public class NarrowCasting {
+  public static void main(String[] args) {
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble; // Explicit casting: double to int
+
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
